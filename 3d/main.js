c1270dd1a31e15bfe9c7fd1689c2cf19bd4a4acf
Yamil Chamo <yajochakha@gmail.com>
2022-11-28 17:26:09 -0600

    Html body, Camera Movement, Event Listener/CSS is next

--- 3d/main.js
+++ 3d/main.js
@@ -1,30 +1,22 @@
 import './style.css'
-
 import * as THREE from 'three';
-
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
-
-
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
-});
+const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#bg'),});
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 renderer.render(scene, camera);
-
+// Torus
 const geometry = new THREE.TorusGeometry(15, 3, 16, 6);
 const material= new THREE.MeshStandardMaterial({color: 0xd3d300});
 const torus = new THREE.Mesh(geometry, material);
 
-scene.add(torus);
+// scene.add(torus);
 
 // Cube
 const nesTexture = new THREE.TextureLoader().load('images/nes.jpg');
@@ -34,25 +26,68 @@ const nes = new THREE.Mesh(
   new THREE.MeshBasicMaterial({ map: nesTexture})
 );
 
-scene.add(nes);
+// scene.add(nes);
 
+// Sun
+
+// const spotLight = new THREE.SpotLight( 0xffffff );
+// spotLight.position.set( 100, 1000, 100 );
+
+// spotLight.castShadow = true;
+
+// spotLight.shadow.mapSize.width = 1024;
+// spotLight.shadow.mapSize.height = 1024;
+
+// spotLight.shadow.camera.near = 500;
+// spotLight.shadow.camera.far = 4000;
+// spotLight.shadow.camera.fov = 30;
+
+// scene.add( spotLight );
 
 // Lighting
-const pointLight = new THREE.PointLight(0xffffff);
+const pointLight = new THREE.PointLight(0xffff80);
 
-pointLight.position.set(5, 5, 5);
+pointLight.position.set(10,10,10);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight)
 
+// Helpers
 const lightHelper = new THREE.PointLightHelper(pointLight)
 const gridHelper = new THREE.GridHelper(200, 50);
-// scene.add(lightHelper, gridHelper)
+scene.add(lightHelper, gridHelper)
 
+// orbitControls
 const controls = new OrbitControls(camera, renderer.domElement);
 
+// Earth
+const earthTexture = new THREE.TextureLoader().load('images/earthDay.jpg');
+const normalEarthTexture = new THREE.TextureLoader().load('images/normalEarth.jpg');
+
+const earth = new THREE.Mesh(
+  new THREE.SphereGeometry(10, 32, 16),
+  new THREE.MeshStandardMaterial({ 
+    map: earthTexture,
+    normalMap: normalEarthTexture
+  } )
+);
+
+earth.position.z = 10;
+
+scene.add(earth)
+
+// Moon
+// const moonTexture = new THREE.TextureLoader().load('images/mn.jpg');
+// const normalEarthTexture = new THREE.TextureLoader().load('images/earthNight.jpg');
+
+// const moon = new THREE.Mesh(
+//   new THREE.SphereGeometry(10, 32, 16),
+//   new THREE.MeshStandardMaterial({ map: earthTexture,
+//   } )
+// );
 
 
+// Stars
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
   const material = new THREE.MeshStandardMaterial({color: 0xcc0000});
@@ -67,16 +102,35 @@ function addStar() {
 
 Array(200).fill().forEach(addStar)
 
+// Background
 const spaceTexture = new THREE.TextureLoader().load('images/space.jpg');
 scene.background = spaceTexture;
 
+// cameraMovement
+function CamMov() {
 
+  const t = document.body.getBoundingClientRect().top;
+  earth.rotation.z += 0.0;
+  earth.rotation.x += 0.00;
+  earth.rotation.y += -0.00;
 
+  camera.position.z = t * -0.03;
+  camera.position.x = t * -0.0;
+  camera.position.y = t * -0.002;
+}
+
+document.body.onscroll = CamMov
+
+// Animation
 function animate() {
   requestAnimationFrame(animate);
-  nes.rotation.x += 0.1;
-  nes.rotation.y += 0.02;
-  nes.rotation.z += 0.01;
+  nes.rotation.x += 0.007;
+  nes.rotation.y += 0.005;
+  nes.rotation.z += 0.0;
+
+  earth.rotation.x += 0.00;
+  earth.rotation.y += 0.01;
+  earth.rotation.z += 0.00;
 
   torus.rotation.x += 0.0;
   torus.rotation.y += 0.02;
