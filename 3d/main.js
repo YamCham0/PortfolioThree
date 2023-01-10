d42e77f9ef08831451327b1d0d2b7af67c322e79
Yamil Chamo <yajochakha@gmail.com>
2023-01-09 19:53:49 -0600

    bootstrap maybe?

--- 3d/main.js
+++ 3d/main.js
@@ -1,7 +1,8 @@
-import './style.css'
+import './style.css';
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-
+import bootstrap from 'bootstrap';
+// ADD carousell from Bootstrap with links and images to the projects
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#bg'),});
